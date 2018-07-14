2103f10a678e57c4660018a4ffa1c7b99fc3b695
XNAR-x4 <38725470+XNAR-x4@users.noreply.github.com>
2018-07-14 11:48:24 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -232,6 +232,22 @@ function play(guild, song) {
 
 	serverQueue.textChannel.send(`بدء تشغيل: **${song.title}**`);
 }
+client.on('message', message => {
+    if (message.content.startsWith("s!avatar")) {
+        var mentionned = message.mentions.users.first();
+        var x5bzm;
+        if (mentionned) {
+            var x5bzm = mentionned;
+        } else {
+            var x5bzm = message.author;
+
+        }
+        const embed = new Discord.RichEmbed()
+            .setColor("RANDOM")
+            .setImage(`${x5bzm.avatarURL}`)
+        message.channel.sendEmbed(embed);
+    }
+});
 client.on("message", message => {
     if (message.content === "s!help") {
         message.reply('**تم ارسال اوامر البوت في الخاص :envelope_with_arrow:**')
