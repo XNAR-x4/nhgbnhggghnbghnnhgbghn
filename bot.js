5cff86205ae844556da5d3e04ae42da619218d7f
XNAR-x4 <38725470+XNAR-x4@users.noreply.github.com>
2018-07-14 17:23:29 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -18,25 +18,32 @@ const queue = new Map();
 
 
 
-
-client.on('ready', function () {
-    client.user.setStatus("online");
-    var ms = 100000;
-    var setGame = [`✧ s!help ✧`];
-    var i = -1;
-    var j = 0;
-    setInterval(function () {
-        if (i == -1) {
-            j = 1;
-        }
-        if (i == (setGame.length) - 1) {
-            j = -1;
-        }
-        i = i + j;
-        client.user.setGame(setGame[i], `✧ s!help ✧`);
-    }, ms); 100000
-
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+client.user.setGame(`✧ s!help ✧`,"http://twitch.tv/S-F")
+  console.log('')
+  console.log('')
+  console.log('╔[═════════════════════════════════════════════════════════════════]╗')
+  console.log(`[Start] ${new Date()}`);
+  console.log('╚[═════════════════════════════════════════════════════════════════]╝')
+  console.log('')
+  console.log('╔[════════════════════════════════════]╗');
+  console.log(`Logged in as * [ " ${client.user.username} " ]`);
+  console.log('')
+  console.log('Informations :')
+  console.log('')
+  console.log(`servers! [ " ${client.guilds.size} " ]`);
+  console.log(`Users! [ " ${client.users.size} " ]`);
+  console.log(`channels! [ " ${client.channels.size} " ]`);
+  console.log('╚[════════════════════════════════════]╝')
+  console.log('')
+  console.log('╔[════════════]╗')
+  console.log(' Bot Is Online')
+  console.log('╚[════════════]╝')
+  console.log('')
+  console.log('')
 });
+
 
 
 
